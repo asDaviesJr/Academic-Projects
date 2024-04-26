e8941c55b20c4906dcb10ed1f42d37bad99bfd26
asDaviesJr <asd1520@rit.edu>
2024-04-26 03:04:42 -0400

    wrong github link used

--- addo-portfolio-main/src/app/pages/Footer.jsx
+++ addo-portfolio-main/src/app/pages/Footer.jsx
@@ -15,7 +15,7 @@ const Footer = () => {
       
       <div className="flex space-x-4">
       <br />
-          <IconLink platform="github" url="https://github.com/asd1520" label="GitHub" />
+          <IconLink platform="github" url="https://github.com/asDaviesJr/Academic-Projects" label="GitHub" />
           <IconLink platform="linkedin" url="https://www.linkedin.com/in/addo-davies-jr/" label="LinkedIn"/>
           <IconLink platform = "email" url="mailto:addo.sdavies@gmail.com" label="Email"/>
           <IconLink platform="resume" url="/Davies_Addo_SE_Resume.pdf" label="Resume"/>
